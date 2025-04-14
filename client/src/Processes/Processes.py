1751386cad52604ac2b6707e106b5661681846e9
Robertas_Vasiliauskas <robertaswasiliauskas@gmail.com>
2025-04-14 23:12:25 +0200

    Add Processes class to retrieve system process information

---
+++ client/src/Processes/Processes.py
@@ -0,0 +1,39 @@
+import psutil
+class Processes:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_count_processes():
+        """
+        Get the number of processes running on the system.
+        """
+
+        try:
+            return len(psutil.pids())
+        except Exception as e:
+            raise RuntimeError(f"An error occurred while retrieving process count: {e}")
+
+    @staticmethod
+    def get_processes(n: int = None) -> list[dict]:
+        """
+        Get a list of processes running on the system.
+        If n is specified, return only the first n processes.
+
+        :param n: Optional; the number of processes to return.
+        :return: A list of dictionaries containing process information.
+        """
+        processes = []
+        try:
+            for proc in psutil.process_iter(['pid', 'name', 'username']):
+                try:
+                    processes.append(proc.info)
+                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                    continue
+        except Exception as e:
+            raise RuntimeError(f"An error occurred while retrieving processes: {e}")
+
+        if n is not None:
+            return processes[:n]
+
+        return processes
