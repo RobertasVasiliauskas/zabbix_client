b9a6ae79aa14fedc5bddf457326f2a735ffe0347
Robertas_Vasiliauskas <robertaswasiliauskas@gmail.com>
2025-04-14 21:36:32 +0200

    Add get_per_core_usage method to CPU class for per-core CPU usage statistics

--- client/src/CPU/CPU.py
+++ client/src/CPU/CPU.py
@@ -12,7 +12,15 @@ class CPU:
         :param interval: Time interval in seconds
         :return: CPU usage percentage
         """
-        return psutil.cpu_percent(interval=1)
+        return psutil.cpu_percent(interval=interval)
+
+    @staticmethod
+    def get_per_core_usage(interval: int) -> list:
+        """
+        Get the CPU usage per core.
+        :return: List of CPU usage percentages for each core
+        """
+        return psutil.cpu_percent(interval=interval, percpu=True)
 
     @staticmethod
     def get_load_average() -> tuple:
